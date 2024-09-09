471d7d0820397a71f0049fe61ae852d4515aa02d
folders64 <kiyan.pavelka@gmail.com>
2024-09-09 20:01:35 +0200

    Added cursor and loading animation GSAP

--- plugins/gsap.js
+++ plugins/gsap.js
@@ -1,3 +1,8 @@
+// plugins/gsap.js
 import Vue from 'vue';
 import gsap from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin';
+
+gsap.registerPlugin(TextPlugin);
+
 Vue.prototype.$gsap = gsap;
